a874f4a1241e37b7c7868c462d0c833837a5af69
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-01-04 12:06:39 -0600

    feat(render-markdown): add table borders and set horizontal rule width to 88

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -2,10 +2,13 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {
     completions = { lsp = { enabled = true } },
+    pipe_table = { preset = "round", border_virtual = true },
+    dash = { width = 88 },
+    -- indent = { enabled = true, skip_heading = true },
   },
 }
