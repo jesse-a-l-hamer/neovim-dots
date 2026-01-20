43a36cd92c092f84fd520154735c1af016cfb8e3
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-01-20 11:48:17 -0600

    feat(noice): use noice to stylize markdown

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -8,6 +8,10 @@ return {
   ---@type NoiceConfig
   opts = {
     lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+      },
       hover = { enabled = false },
       signature = { enabled = false },
     },
