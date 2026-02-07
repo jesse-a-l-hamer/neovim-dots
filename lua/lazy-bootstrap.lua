9524f4b636e05386e9fb9ab66990f5fd1683c2d5
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-02-07 07:43:23 -0600

    chore(lazy-bootstrap): bring in changes from upstream kickstart repo

--- lua/lazy-bootstrap.lua
+++ lua/lazy-bootstrap.lua
@@ -1,7 +1,7 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system {
     "git",
@@ -12,7 +12,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     lazypath,
   }
   if vim.v.shell_error ~= 0 then
-    error("Error cloning lazy.nvim:\n" .. out)
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
   end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
