95ded07b0e4a57007694e0208831ef8fbc60d652
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-04-13 15:36:10 -0500

    feat(lsp): fix deprecated client.supports_method call in config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -16,7 +16,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local client = vim.lsp.get_client_by_id(event.data.client_id)
     if
       client
-      and client.supports_method(
+      and client:supports_method(
         vim.lsp.protocol.Methods.textDocument_documentHighlight,
         event.buf
       )
