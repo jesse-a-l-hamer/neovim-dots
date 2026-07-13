402390d5df2c82626246a47094c7c40fdf09a170
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-07-13 07:30:15 -0500

    fix(plugins/blink-pairs.lua): update blink-pairs to use blink.lib

--- lua/plugins/blink-pairs.lua
+++ lua/plugins/blink-pairs.lua
@@ -3,15 +3,12 @@
 --- @type LazyPluginSpec
 return {
   "saghen/blink.pairs",
+  dependencies = "saghen/blink.lib",
   version = "*", -- (recommended) only required with prebuilt binaries
 
-  -- download prebuilt binaries from github releases
-  -- dependencies = 'saghen/blink.download',
-  -- OR build from source, requires nightly:
-  -- https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-  build = "cargo build --release",
-  -- If you use nix, you can build from source using latest nightly rust with:
-  -- build = 'nix run .#build-plugin',
+  build = function()
+    require("blink.pairs").build():pwait(60000)
+  end,
 
   --- @module 'blink.pairs'
   --- @type blink.pairs.Config
