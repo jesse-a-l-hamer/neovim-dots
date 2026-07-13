748456825f111a4961fa1f5c1fb062bd811f8fd4
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-07-13 07:32:11 -0500

    fix(ftplugin/rust.lua): fix broken rustaceanvim keybindings

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,18 +1,22 @@
 local bufnr = vim.api.nvim_get_current_buf()
-vim.keymap.set(
-  "n",
-  "<leader>la",
-  function()
-    vim.cmd.RustLsp('codeAction') -- supports rust-analyzer's grouping
-    -- or vim.lsp.buf.codeAction() if you don't want grouping.
-  end,
-  { silent = true, buffer = bufnr }
-)
-vim.keymap.set(
-  "n",
-  "K",  -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-  function()
-    vim.cmd.RustLsp({'hover', 'actions'})
-  end,
-  { silent = true, buffer = bufnr }
-)
+
+-- KEYMAPS
+vim.keymap.set("n", "<leader>lA", function()
+  vim.cmd.RustLsp "codeAction" -- supports rust-analyzer's grouping
+  -- or vim.lsp.buf.codeAction() if you don't want grouping.
+end, { silent = true, buffer = bufnr, desc = "Code actions (grouped)" })
+
+vim.keymap.set("n", "<leader>lh", function()
+  vim.cmd.RustLsp { "hover", "actions" }
+end, { silent = true, buffer = bufnr, desc = "Hover actions" })
+
+vim.keymap.set("n", "<leader>le", function()
+  vim.cmd.RustLsp { "explainError", "current" }
+end, { silent = true, buffer = bufnr, desc = "Explain errors" })
+
+vim.keymap.set("n", "<leader>lR", function()
+  vim.cmd.RustLsp { "renderDiagnostic", "current" }
+end, { silent = true, buffer = bufnr, desc = "Render diagnostic" })
+
+-- OPTIONS
+vim.o.colorcolumn = "100"
