5d8252332c072ce1e3c56d10f1b724fd02d16a6f
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-07-25 05:20:51 -0500

    feat(treesitter): add support for context and textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,171 +7,171 @@ return {
     build = ":TSUpdate",
     branch = "main",
     lazy = false,
-    -- dependencies = {
-    --   {
-    --     "nvim-treesitter/nvim-treesitter-context",
-    --     lazy = true,
-    --   },
-    --   {
-    --     "nvim-treesitter/nvim-treesitter-textobjects",
-    --     lazy = true,
-    --   },
-    -- },
-    config = function ()
-      local parsers = {
-        "bash",
-        "beancount",
-        "bibtex",
-        "c",
-        "comment",
-        "css",
-        "csv",
-        "desktop",
-        "diff",
-        "faust",
-        "git_config",
-        "gitignore",
-        "go",
-        "gomod",
-        "gpg",
-        "html",
-        "http",
-        "ini",
-        "javascript",
-        "jinja",
-        "jinja_inline",
-        "json",
-        "latex",
-        "lua",
-        "luadoc",
-        "markdown",
-        "markdown_inline",
-        "nginx",
-        "norg",
-        "powershell",
-        "pymanifest",
-        "python",
-        "query",
-        "r",
-        "regex",
-        "ron",
-        "rst",
-        "ruby",
-        "rust",
-        "scss",
-        "sql",
-        "ssh_config",
-        "svelte",
-        "tera",
-        "todotxt",
-        "toml",
-        "tsv",
-        "tsx",
-        "typst",
-        "udev",
-        "vim",
-        "vimdoc",
-        "vue",
-        "xml",
-        "yaml",
-      }
-      require("nvim-treesitter").install(parsers)
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function (args)
-          local buf, filetype = args.buf, args.match
-
-          local language = vim.treesitter.language.get_lang(filetype)
-          if not language then return end
-
-          -- check if parser exists and load it
-          if not vim.treesitter.language.add(language) then return end
-          --enables syntax highlighting and other treesitter features
-          vim.treesitter.start(buf, language)
-
-          --enables treesitter based folds
-          -- for more info on folds see `:help folds`
-          vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
-          vim.wo.foldmethod = 'expr'
-
-          -- enables treesitter based indentation
-          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-        end,
-      })
-    end,
-    -- opts = {
-    --   auto_install = true,
-    --   highlight = {
-    --     enable = true,
-    --     -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-    --     --  If you are experiencing weird indenting issues, add the language to
-    --     --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-    --     additional_vim_regex_highlighting = false, -- Catppuccin reccommends false
-    --   },
-    --   indent = { enable = true, disable = { "ruby" } },
-    --   incremental_selection = { enable = true },
-    --   context = {
-    --     enable = true,
-    --   },
-    --   textobjects = {
-    --     -- all textobjects settings taken from AstroNvim:
-    --     -- https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/treesitter.lua
-    --     select = {
-    --       enable = true,
-    --       lookahead = true,
-    --       keymaps = {
-    --         ["ak"] = { query = "@block.outer", desc = "around block" },
-    --         ["ik"] = { query = "@block.inner", desc = "inside block" },
-    --         ["ac"] = { query = "@class.outer", desc = "around class" },
-    --         ["ic"] = { query = "@class.inner", desc = "inside class" },
-    --         ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-    --         ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-    --         ["af"] = { query = "@function.outer", desc = "around function " },
-    --         ["if"] = { query = "@function.inner", desc = "inside function " },
-    --         ["ao"] = { query = "@loop.outer", desc = "around loop" },
-    --         ["io"] = { query = "@loop.inner", desc = "inside loop" },
-    --         ["aa"] = { query = "@parameter.outer", desc = "around argument" },
-    --         ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-    --       },
-    --     },
-    --     swap = {
-    --       enable = true,
-    --       swap_next = {
-    --         [">K"] = { query = "@block.outer", desc = "Swap next block" },
-    --         [">F"] = { query = "@function.outer", desc = "Swap next function" },
-    --         [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-    --       },
-    --       swap_previous = {
-    --         ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-    --         ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-    --         ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-    --       },
-    --     },
-    --     move = {
-    --       enable = true,
-    --       set_jumps = true,
-    --       goto_next_start = {
-    --         ["]k"] = { query = "@block.outer", desc = "Next block start" },
-    --         ["]f"] = { query = "@function.outer", desc = "Next function start" },
-    --         ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-    --       },
-    --       goto_next_end = {
-    --         ["]K"] = { query = "@block.outer", desc = "Next block end" },
-    --         ["]F"] = { query = "@function.outer", desc = "Next function end" },
-    --         ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
-    --       },
-    --       goto_previous_start = {
-    --         ["[k"] = { query = "@block.outer", desc = "Previous block start" },
-    --         ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-    --         ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
-    --       },
-    --       goto_previous_end = {
-    --         ["[K"] = { query = "@block.outer", desc = "Previous block end" },
-    --         ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-    --         ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
-    --       },
-    --     },
-    --   },
-    -- },
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-context",
+        lazy = true,
+      },
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        lazy = true,
+      },
+    },
+    -- config = function ()
+    --   local parsers = {
+    --     "bash",
+    --     "beancount",
+    --     "bibtex",
+    --     "c",
+    --     "comment",
+    --     "css",
+    --     "csv",
+    --     "desktop",
+    --     "diff",
+    --     "faust",
+    --     "git_config",
+    --     "gitignore",
+    --     "go",
+    --     "gomod",
+    --     "gpg",
+    --     "html",
+    --     "http",
+    --     "ini",
+    --     "javascript",
+    --     "jinja",
+    --     "jinja_inline",
+    --     "json",
+    --     "latex",
+    --     "lua",
+    --     "luadoc",
+    --     "markdown",
+    --     "markdown_inline",
+    --     "nginx",
+    --     "norg",
+    --     "powershell",
+    --     "pymanifest",
+    --     "python",
+    --     "query",
+    --     "r",
+    --     "regex",
+    --     "ron",
+    --     "rst",
+    --     "ruby",
+    --     "rust",
+    --     "scss",
+    --     "sql",
+    --     "ssh_config",
+    --     "svelte",
+    --     "tera",
+    --     "todotxt",
+    --     "toml",
+    --     "tsv",
+    --     "tsx",
+    --     "typst",
+    --     "udev",
+    --     "vim",
+    --     "vimdoc",
+    --     "vue",
+    --     "xml",
+    --     "yaml",
+    --   }
+    --   require("nvim-treesitter").install(parsers)
+    --   vim.api.nvim_create_autocmd("FileType", {
+    --     callback = function (args)
+    --       local buf, filetype = args.buf, args.match
+    --
+    --       local language = vim.treesitter.language.get_lang(filetype)
+    --       if not language then return end
+    --
+    --       -- check if parser exists and load it
+    --       if not vim.treesitter.language.add(language) then return end
+    --       --enables syntax highlighting and other treesitter features
+    --       vim.treesitter.start(buf, language)
+    --
+    --       --enables treesitter based folds
+    --       -- for more info on folds see `:help folds`
+    --       vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    --       vim.wo.foldmethod = 'expr'
+    --
+    --       -- enables treesitter based indentation
+    --       vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    --     end,
+    --   })
+    -- end,
+    opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = false, -- Catppuccin reccommends false
+      },
+      indent = { enable = true, disable = { "ruby" } },
+      incremental_selection = { enable = true },
+      context = {
+        enable = true,
+      },
+      textobjects = {
+        -- all textobjects settings taken from AstroNvim:
+        -- https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/treesitter.lua
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["ak"] = { query = "@block.outer", desc = "around block" },
+            ["ik"] = { query = "@block.inner", desc = "inside block" },
+            ["ac"] = { query = "@class.outer", desc = "around class" },
+            ["ic"] = { query = "@class.inner", desc = "inside class" },
+            ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
+            ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
+            ["af"] = { query = "@function.outer", desc = "around function " },
+            ["if"] = { query = "@function.inner", desc = "inside function " },
+            ["ao"] = { query = "@loop.outer", desc = "around loop" },
+            ["io"] = { query = "@loop.inner", desc = "inside loop" },
+            ["aa"] = { query = "@parameter.outer", desc = "around argument" },
+            ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+          },
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            [">K"] = { query = "@block.outer", desc = "Swap next block" },
+            [">F"] = { query = "@function.outer", desc = "Swap next function" },
+            [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
+          },
+          swap_previous = {
+            ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
+            ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
+            ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,
+          goto_next_start = {
+            ["]k"] = { query = "@block.outer", desc = "Next block start" },
+            ["]f"] = { query = "@function.outer", desc = "Next function start" },
+            ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+          },
+          goto_next_end = {
+            ["]K"] = { query = "@block.outer", desc = "Next block end" },
+            ["]F"] = { query = "@function.outer", desc = "Next function end" },
+            ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+          },
+          goto_previous_start = {
+            ["[k"] = { query = "@block.outer", desc = "Previous block start" },
+            ["[f"] = { query = "@function.outer", desc = "Previous function start" },
+            ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+          },
+          goto_previous_end = {
+            ["[K"] = { query = "@block.outer", desc = "Previous block end" },
+            ["[F"] = { query = "@function.outer", desc = "Previous function end" },
+            ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+          },
+        },
+      },
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
