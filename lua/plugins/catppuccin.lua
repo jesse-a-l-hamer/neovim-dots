b09978dbe58ffb489d50794e52d48df739606b00
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-03 00:12:19 -0500

    feat(plugins/catppuccin): revert most settings back to defaults

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,4 +1,4 @@
-local accent_color = "flamingo"
+-- local accent_color = "flamingo"
 
 ---@module 'lazy'
 ---@module 'catppuccin'
@@ -22,44 +22,45 @@ return {
       shade = "dark",
       percentage = 0.15,
     },
-    transparent_background = true,
-    float = { transparent = true },
+    -- transparent_background = true,
+    -- float = { transparent = true },
     auto_integrations = true,
-    lsp_styles = {
-      virtual_text = {
-        errors = { "bold", "italic" },
-        hints = { "bold", "italic" },
-        warnings = { "bold", "italic" },
-        information = { "bold", "italic" },
-        ok = { "bold", "italic" },
-      },
-      underlines = {
-        errors = { "undercurl" },
-        hints = { "undercurl" },
-        warnings = { "undercurl" },
-        information = { "undercurl" },
-        ok = { "undercurl" },
-      },
-      inlay_hints = {
-        background = true,
-      },
-    },
-    custom_highlights = function(colors)
-      ---@type table<string, CtpHighlight>
-      return {
-        -- globals
-        CursorLineNr = { fg = colors[accent_color] },
-        FloatBorder = { fg = colors[accent_color] },
-        Title = { fg = colors[accent_color] },
-        BlinkCmpMenuBorder = { fg = colors[accent_color] },
-        BlinkCmpDocBorder = { fg = colors[accent_color] },
-        BlinkCmpSignatureHelpBorder = { fg = colors[accent_color] },
-      }
-    end,
-    integrations = {
-      blink_cmp = { style = "bordered" },
-      colorful_winsep = { color = accent_color },
-      snacks = { indent_scope_color = accent_color },
-    },
+    term_colors = true,
+    -- lsp_styles = {
+    --   virtual_text = {
+    --     errors = { "bold", "italic" },
+    --     hints = { "bold", "italic" },
+    --     warnings = { "bold", "italic" },
+    --     information = { "bold", "italic" },
+    --     ok = { "bold", "italic" },
+    --   },
+    --   underlines = {
+    --     errors = { "undercurl" },
+    --     hints = { "undercurl" },
+    --     warnings = { "undercurl" },
+    --     information = { "undercurl" },
+    --     ok = { "undercurl" },
+    --   },
+    --   inlay_hints = {
+    --     background = true,
+    --   },
+    -- },
+    -- custom_highlights = function(colors)
+    --   ---@type table<string, CtpHighlight>
+    --   return {
+    --     -- globals
+    --     CursorLineNr = { fg = colors[accent_color] },
+    --     FloatBorder = { fg = colors[accent_color] },
+    --     Title = { fg = colors[accent_color] },
+    --     BlinkCmpMenuBorder = { fg = colors[accent_color] },
+    --     BlinkCmpDocBorder = { fg = colors[accent_color] },
+    --     BlinkCmpSignatureHelpBorder = { fg = colors[accent_color] },
+    --   }
+    -- end,
+    -- integrations = {
+    --   blink_cmp = { style = "bordered" },
+    --   colorful_winsep = { color = accent_color },
+    --   snacks = { indent_scope_color = accent_color },
+    -- },
   },
 }
