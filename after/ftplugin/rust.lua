9005bdd356e9230781753068cd46baf42a4ff428
Jesse Hamer <jesse.a.l.hamer@gmail.com>
2026-08-06 12:13:11 -0500

    feat(rustaceanvim): configure test/debug keybinds and neotest adapter

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -18,5 +18,35 @@ vim.keymap.set("n", "<leader>lR", function()
   vim.cmd.RustLsp { "renderDiagnostic", "current" }
 end, { silent = true, buffer = bufnr, desc = "Render diagnostic" })
 
+vim.keymap.set("n", "<leader>lt", function()
+  vim.cmd.RustLsp { "relatedTests" }
+end, { silent = true, buffer = bufnr, desc = "Find related tests" })
+
+vim.keymap.set("n", "<leader>lT", function()
+  vim.cmd.RustLsp { "testables" }
+end, { silent = true, buffer = bufnr, desc = "Show testables" })
+
+vim.keymap.set("n", "<leader>lc", function()
+  vim.cmd.RustLsp { "openCargo" }
+end, { silent = true, buffer = bufnr, desc = "Open Cargo.toml" })
+
+require("which-key").add { "<leader>dR", group = "Rustaceanvim" }
+vim.keymap.set("n", "<leader>dRd", function()
+  vim.cmd.RustLsp { "debug" }
+end, { silent = true, buffer = bufnr, desc = "Debug at cursor" })
+vim.keymap.set("n", "<leader>dRD", function()
+  vim.cmd.RustLsp { "debuggables" }
+end, { silent = true, buffer = bufnr, desc = "See all debuggables" })
+
 -- OPTIONS
 vim.o.colorcolumn = "100"
+
+-- NEOTEST
+require("neotest").setup {
+  adapters = {
+    require "rustaceanvim.neotest",
+  },
+}
+
+-- RUSTACEANVIM CONFIG
+---@type rustaceanvim.Config
--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -5,4 +5,11 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^9",
   lazy = false, -- plugin implements its own lazy-loading; no need to use lazy.nvim
+  config = function(opts)
+    vim.g.rustaceanvim = {
+      tools = {
+        test_executor = "background",
+      },
+    }
+  end,
 }
